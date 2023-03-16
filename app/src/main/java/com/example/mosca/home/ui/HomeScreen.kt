4a39c75a88c2c026aa28ca5cccca17436eceab88
honeybadger2788 <noe.carosella@gmail.com>
2023-03-15 21:10:47 -0300

    add expenseDialog

--- app/src/main/java/com/example/mosca/home/ui/HomeScreen.kt
+++ app/src/main/java/com/example/mosca/home/ui/HomeScreen.kt
@@ -1,26 +1,40 @@
 package com.example.mosca.home.ui
 
+import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material.icons.outlined.ArrowUpward
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 
 @Preview(showBackground = true)
 @Composable
 fun HomeScreen(){
+    var show by rememberSaveable {
+        mutableStateOf(false)
+    }
     Box(
         Modifier
             .fillMaxSize()) {
+        AddExpensesDialog(show, onDismiss = { show =  false })
         Column {
             TopBar()
             Budget(
@@ -29,7 +43,7 @@ fun HomeScreen(){
                     .padding(16.dp))
             ExpensesList(Modifier.padding(16.dp))
         }
-        FabDialog(Modifier.align(Alignment.BottomEnd))
+        FabDialog(Modifier.align(Alignment.BottomEnd)) { show = true }
     }
 }
 
@@ -48,7 +62,7 @@ fun TopBar() {
 @Composable
 fun ExpensesList(modifier: Modifier) {
     Column(modifier = modifier) {
-        Text(text = "Movimientos:", fontSize = 18.sp)
+        Text(text = "Movimientos:", fontSize = 18.sp, modifier = Modifier.padding(bottom = 8.dp))
         LazyColumn{
             items(20){
                 ItemExpense()
@@ -64,7 +78,7 @@ fun ItemExpense() {
             .fillMaxWidth()
             .padding(vertical = 8.dp, horizontal = 16.dp), verticalAlignment = Alignment.CenterVertically) {
         Icon(imageVector = Icons.Outlined.ArrowUpward, contentDescription = "")
-        Spacer(modifier = Modifier.width(8.dp))
+        Spacer(modifier = Modifier.size(8.dp))
         Text(text = "Gasto $0.00")
     }
 }
@@ -82,8 +96,8 @@ fun Budget(modifier: Modifier) {
 }
 
 @Composable
-fun FabDialog(modifier: Modifier) {
-    FloatingActionButton(onClick = { },
+fun FabDialog(modifier: Modifier, showDialog: () -> Unit) {
+    FloatingActionButton(onClick = { showDialog() },
         modifier = modifier.padding(16.dp)
     ) {
         Icon(imageVector = Icons.Filled.Add, contentDescription = "")
@@ -91,6 +105,57 @@ fun FabDialog(modifier: Modifier) {
 }
 
 @Composable
-fun AddExpensesDialog(show: Boolean, onDismiss: () -> Unit, onTaskAdded: () -> Unit) {
-    TODO("Not yet implemented")
+fun AddExpensesDialog(show: Boolean, onDismiss: () -> Unit) {
+    var detail by rememberSaveable {
+        mutableStateOf("")
+    }
+
+    var amount by rememberSaveable {
+        mutableStateOf("")
+    }
+
+    if(show){
+        Dialog(onDismissRequest = { onDismiss() }) {
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .padding(16.dp)) {
+                Text(
+                    text = "Agregar movimiento",
+                    fontSize = 18.sp,
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.size(16.dp))
+                Row {
+                    TextField(
+                        modifier = Modifier.weight(2f),
+                        placeholder = { Text(text = "Detalle") },
+                        value = detail,
+                        onValueChange = { detail = it },
+                        singleLine = true,
+                        maxLines = 1
+                    )
+                    Spacer(modifier = Modifier.size(8.dp))
+                    TextField(
+                        modifier = Modifier.weight(1f),
+                        placeholder = { Text(text = "$") },
+                        value = amount,
+                        onValueChange = { amount = it },
+                        singleLine = true,
+                        maxLines = 1,
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                    )
+                }
+                Spacer(modifier = Modifier.size(16.dp))
+                Button(
+                    onClick = { Log.i("Noe", "Info enviada ${detail} ${amount}")  },
+                    Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Agregar")
+                }
+            }
+        }
+    }
 }
