2d61ca629b010bc8334372138e6ddf579ff43025
honeybadger2788 <noe.carosella@gmail.com>
2023-03-17 16:28:54 -0300

    create CustomTextField

---
+++ app/src/main/java/com/example/mosca/home/ui/composable/CustomTextField.kt
@@ -0,0 +1,40 @@
+package com.example.mosca.home.ui.composable
+
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
+
+@Composable
+fun CustomTextFieldOutlined(
+    label: String,
+    textValue: String,
+    onTextChanged: (String) -> Unit,
+    trailingIcon: @Composable () -> Unit = {},
+    modifier: Modifier,
+    visualTransformation: VisualTransformation = VisualTransformation.None
+) {
+    OutlinedTextField(
+        value = textValue ,
+        onValueChange = { onTextChanged(it) },
+        modifier = modifier,
+        label = { Text(text = label) },
+        maxLines = 1,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            textColor = Color.DarkGray,
+            backgroundColor = Color(0xFFFAFAFA),
+            focusedBorderColor = Color(0xff00bcd4),
+            unfocusedLabelColor = Color(0xFFB2B2B2),
+            focusedLabelColor = Color(0xff00bcd4)
+        ),
+        trailingIcon = trailingIcon,
+        visualTransformation = visualTransformation
+    )
+}
