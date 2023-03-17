e82c46c26acbe7f7924de8769d1843638c4ebca8
honeybadger2788 <noe.carosella@gmail.com>
2023-03-17 17:00:06 -0300

    create default button

---
+++ app/src/main/java/com/example/mosca/ui/composable/DefaultButton.kt
@@ -0,0 +1,26 @@
+package com.example.mosca.ui.composable
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun DefaultButton(text: String, onClick: () -> Unit, enabled: Boolean) {
+    Button(
+        onClick = onClick,
+        enabled = enabled,
+        modifier = Modifier.fillMaxWidth(),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Color(0xff0097a7),
+            disabledBackgroundColor = Color(0xFF80deea),
+            contentColor = Color.White,
+            disabledContentColor = Color.White
+        )
+    ) {
+        Text(text = text)
+    }
+}
